66d2030bc31253b60cbd393699a9169f9a0bc95c
Amartya Mondal <atm1504.in@gmail.com>
2023-06-22 10:21:14 +0530

    Assets and outlets added

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var recordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        print("viewWillapear call");
+    }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning();
+        print("did receive memeory warning");
+    }
+    
+    
     @IBAction func recordAudio(_ sender: Any) {
         print("Record button was pressed");
         recordingLabel.text="Recording audio";
